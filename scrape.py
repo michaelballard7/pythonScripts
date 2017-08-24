e98d4117fea83621676a4052e80a5d579af8e222
Michael Ballard <ballard.michael9@gmail.com>
2017-08-24 10:45:36 -0400

    made get request dynamic

--- scrape.py
+++ scrape.py
@@ -2,7 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 # establish the target URL or URL's
-url = "https://www.yelp.com/search?find_desc=&find_loc=San+Francisco%2C+CA&ns=1"
+base_url = "https://www.yelp.com/search?find_desc=&find_loc="
+location = "New York, NY"
+url = base_url + location + "&start=" + str(search_pg)
+search_pg = 0
 
 # utilize the request get method to get the target url page
 yelp_req = requests.get(url)
@@ -11,22 +14,22 @@ yelp_req = requests.get(url)
 yelp_soup = BeautifulSoup(yelp_req.text, 'html.parser')
 
 # use the status_code method to from requests library to check for 200 connection
-print(yelp_req.status_code)
-
-# prints out raw HTML text from the target site
-print(yelp_req.text)
-
-# The beautifulsoup prettify method allows me to beautify the text extraction from target
-print(yelp_soup.prettify())
-
-# use methods to target specific element on a page
-print(yelp_soup.title)
-
-# use the find all method to target all iterations of a specific elements
-print(yelp_soup.find_all('p'))
-
+print(" My status code is: " + str(yelp_req.status_code))
+#
+# # prints out raw HTML text from the target site
+# print(yelp_req.text)
+#
+# # The beautifulsoup prettify method allows me to beautify the text extraction from target
+# print(yelp_soup.prettify())
+#
+# # use methods to target specific element on a page
+# print(yelp_soup.title)
+#
+# # use the find all method to target all iterations of a specific elements
+# print(yelp_soup.find_all('p'))
+#
 # build a loop to iterate through all the links on the target site
-for link in yelp_soup.find_all('a'):
-    print(link.get('href'))
+# for link in yelp_soup.find_all('a'):
+#     print(link)
 
 # I can use the find method instead of find_all
